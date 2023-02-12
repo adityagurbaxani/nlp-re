ba38fd38646135730e823034d0bd6a48606782ed
Aditya Gurbaxani <agurbaxani@Adityas-MacBook-Pro.local>
2023-02-12 13:19:58 +0530

    Basic code to read text from file

--- code.py
+++ code.py
@@ -8,10 +8,37 @@ Created on Sat Feb 11 22:52:50 2023
 
 import pandas as pd
 import numpy as np
+import urllib
 
-#Create a DataFrame to process the text
+#target_url = 'https://raw.githubusercontent.com/adityagurbaxani/nlp-re/main/Data/nfr.txt'
 
-filepath = 'https://raw.githubusercontent.com/adityagurbaxani/nlp-re/2483a5a3f99c211bd770c73c72376d07c45d7dcf/Data/nfr.txt'
-with open(filepath,'r') as f:
+#for line in urllib.request.urlopen(target_url):
+    #print(type(line.decode('utf-8')))
+    #lines.append(line.decode('utf_16'))
+
+#Reading the text data from file
+lines = []
+with open('./Data/nfr.txt') as f:
     lines = f.readlines()
-    print(lines)
+
+
+for i,l in enumerate(lines):
+    print(i,l)
+    
+#Create a DataFrame to process the text
+df = pd.DataFrame()
+df['Raw'] = lines
+
+
+#Split each requirement into req-label & req
+req_labels = []
+req_texts =[]
+
+for i in range(df.shape[0]):
+    req = df.iloc[i,0]
+    sep_index = req.find(":")
+    req_labels.append(req[:sep_index])
+    req_texts.append(req[sep_index+1:])
+
+df['req-label'] = req_labels
+df['req'] = req_texts
